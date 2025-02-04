2c7aa16187924e33ef3db848c79e15e98f8ff189
raffayuda <rafffa0206@gmail.com>
2025-02-04 10:38:25 +0700

    commit

--- components/banner.tsx
+++ components/banner.tsx
@@ -9,9 +9,10 @@ const Banner: React.FC<BannerProps> = ({ data }) => {
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div
         className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
-        style={{ backgroundImage: `url(${data?.imageUrl})` }}
+        style={{ backgroundImage: `url(${data?.imageUrl ? data.imageUrl : "https://img.freepik.com/premium-photo/female-buyer-pays-with-bank-card-tea-shop-happy-seller-smiling-excellent-service_1048944-3428350.jpg?uid=R72249137&ga=GA1.1.5617288.1734018438&semt=ais_hybrid_sidr"})` }}
       >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
+         
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data?.label}
           </div>
